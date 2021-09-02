97a5f29d57451ea437a6f395baff03b6ec16208e
SamsAgency <samstore637@gmail.com>
2021-09-02 21:32:01 +0300

    converted location to city and country schemas

--- models/admin.js
+++ models/admin.js
@@ -9,34 +9,39 @@ const adminSchema = new Schema({
     firstname : {
         type: String,
         required: true,
-        minlength: 5,
+        minlength: 4,
         maxlength: 100
     },
     lastname : {
         type: String,
         required: true,
-        minlength: 5,
+        minlength: 4,
         maxlength: 100
     },
     username : {
         type: String,
         unique: true,
         required: true,
-        minlength: 5,
+        minlength: 4,
         maxlength: 100
     },
-    location: {
+    city: {
         type: String,
         required: true,
-        minlength: 5,
-        maxlength: 100
-        
+        minlength: 4,
+        maxlength: 30
+    },
+    country: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 30
     },
     email: {
         type: String,
         required: true,
         unique: true,
-        minlength: 5,
+        minlength: 4,
         maxlength: 100
     },
     password: {
@@ -69,7 +74,8 @@ const validateAdmin = (admin) => {
         firstname: Joi.string().required().min(5).max(100),
         lastname: Joi.string().required().min(5).max(100),
         username: Joi.string().required().min(5).max(100),
-        location: Joi.string().required().min(5).max(100),
+        city: Joi.string().required().min(4).max(30),
+        country: Joi.string().required().min(4).max(30),
         email: Joi.string().required().min(5).max(100).email({minDomainSegments: 2, tlds : { allow : ['com', 'net', 'org'] }}),
         password: Joi.string().required().min(5).max(100).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
     })
